b09180c01392b7f3de10059d2a91fef581939e93
linyunyi <471177894@qq.com>
2024-08-29 18:03:37 +0800

    chore: 新增pdf上传和显示功能

--- src/sidepanel/index.ts
+++ src/sidepanel/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { setupConnection, MESSAGE_PORT_NAME, EVENT_TYPE } from '../messaging/index';
 
 const {send} = setupConnection(MESSAGE_PORT_NAME.SIDEPANEL);
@@ -8,6 +9,22 @@ const getJobContent = async () => {
     const jobContent = document.querySelector('#job-content')
     if(jobContent)jobContent.innerHTML = data
 }
+const fileInput = document.getElementById('fileInput');
+
+if(fileInput){
+    fileInput.addEventListener('change', (event)=> {
+        console.log("change",event)
+        const file = event?.target?.files[0];
+        if (file && file.type === 'application/pdf') {
+            const fileURL = URL.createObjectURL(file);
+            console.log('File URL:', fileURL);
+            document.getElementById('pdfPreview').src = fileURL;
+        } else {
+            alert('Please select a PDF file.');
+        }
+    });
+}
+
 
 (async () => {
     getJobContent()
